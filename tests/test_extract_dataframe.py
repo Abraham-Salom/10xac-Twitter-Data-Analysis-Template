66cecbb9c2d649fb86b56b92f49a359fca665023
Abraham-Salom <u11252759@tuks.co.za>
2022-08-10 18:48:03 +0200

    unitesting

--- tests/test_extract_dataframe.py
+++ tests/test_extract_dataframe.py
@@ -1,8 +1,9 @@
 import unittest
 import pandas as pd
 import sys, os
+import json
 
-sys.path.append(os.path.abspath(os.path.join("../..")))
+sys.path.append(os.path.abspath(os.path.join("../")))
 
 from extract_dataframe import read_json
 from extract_dataframe import TweetDfExtractor
@@ -11,7 +12,8 @@ from extract_dataframe import TweetDfExtractor
 # we will need about 5 tweet samples. 
 # Create a sample not more than 10 tweets and place it in a json file.
 # Provide the path to the samples tweets file you created below
-sampletweetsjsonfile = ""   #put here the path to where you placed the file e.g. ./sampletweets.json. 
+#f  = open(Sample_Tweeter.json)
+sampletweetsjsonfile = 'Sample_Tweeter.json'  #put here the path to where you placed the file e.g. ./sampletweets.json. 
 _, tweet_list = read_json(sampletweetsjsonfile)
 
 columns = [
@@ -36,7 +38,6 @@ columns = [
     "place_coord_boundaries",
 ]
 
-
 class TestTweetDfExtractor(unittest.TestCase):
     """
 		A class for unit-testing function in the fix_clean_tweets_dataframe.py file
@@ -47,17 +48,20 @@ class TestTweetDfExtractor(unittest.TestCase):
 			from the unittest module
 	"""
 
+
     def setUp(self) -> pd.DataFrame:
         self.df = TweetDfExtractor(tweet_list[:5])
         # tweet_df = self.df.get_tweet_df()
 
     def test_find_statuses_count(self):
         self.assertEqual(
-            self.df.find_statuses_count(), <provide a list of the first five status counts>
+            self.df.find_statuses_count(), #<provide a list of the first five status counts>
         )
 
     def test_find_full_text(self):
-        text = <provide a list of the first five full texts>
+        #text = <provide a list of the first five full texts>
+        text = tweet_list['clean_text'].head(5)
+        #text
 
         self.assertEqual(self.df.find_full_text(), text)
 
@@ -65,36 +69,38 @@ class TestTweetDfExtractor(unittest.TestCase):
         self.assertEqual(
             self.df.find_sentiments(self.df.find_full_text()),
             (
-                <provide a list of the first five sentiment values>,
-                <provide a list of the first five polarity values>,
+                #<provide a list of the first five sentiment values>,
+                [0,0,0,0,0], [0,0,0,0,0]
+                #<provide a list of the first five polarity values>,
+                [0,1,0,1,1], [0,1,1,0,0]
             ),
         )
 
-
     def test_find_screen_name(self):
-        name = <provide a list of the first five screen names>
+        #name = <provide a list of the first five screen names>
+        name = tweet_list['original_author'].head(5)
         self.assertEqual(self.df.find_screen_name(), name)
 
     def test_find_followers_count(self):
-        f_count = <provide a list of the first five follower counts>
+        #f_count = <provide a list of the first five follower counts>
+        f_count = tweet_list['followers_count'].head(5)
         self.assertEqual(self.df.find_followers_count(), f_count)
 
     def test_find_friends_count(self):
-        friends_count = <provide a list of the first five friend's counts>
+        #friends_count = <provide a list of the first five friend's counts>
+        friends_count = tweet_list['friends_count'].head(5)
         self.assertEqual(self.df.find_friends_count(), friends_count)
 
     def test_find_is_sensitive(self):
-        self.assertEqual(self.df.is_sensitive(), <provide a list of the first five is_sensitive values>)
+        self.assertEqual(self.df.is_sensitive() [True, False, True, True, False]) #<provide a list of the first five is_sensitive values>)
 
 
-    # def test_find_hashtags(self):
+    #def test_find_hashtags(self):
     #     self.assertEqual(self.df.find_hashtags(), )
 
     # def test_find_mentions(self):
     #     self.assertEqual(self.df.find_mentions(), )
 
-
-
-if __name__ == "__main__":
-    unittest.main()
+    if __name__ == "__main__":
+        unittest.main()
 
